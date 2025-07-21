536710bb433c9e9ff5bab38dd8a1df878721fe33
CroakBot1 <apploverss1@gmail.com>
2025-07-21 09:19:09 +0800

    Update logger.js

--- src/backend-trader/logger.js
+++ src/backend-trader/logger.js
@@ -1,1 +1,11 @@
+function info(...args) {
+  console.log('[ℹ️ INFO]', ...args);
+}
+function warn(...args) {
+  console.warn('[⚠️ WARN]', ...args);
+}
+function error(...args) {
+  console.error('[❌ ERROR]', ...args);
+}
+module.exports = { info, warn, error };
 
