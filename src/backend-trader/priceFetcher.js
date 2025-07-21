fd552beea86683e0c8c660ae5192465cffff7aaa
CroakBot1 <apploverss1@gmail.com>
2025-07-21 09:18:50 +0800

    Update priceFetcher.js

--- src/backend-trader/priceFetcher.js
+++ src/backend-trader/priceFetcher.js
@@ -1,1 +1,9 @@
+const axios = require('axios');
+
+async function fetchBybitPrice() {
+  const res = await axios.get('https://api.bybit.com/v2/public/tickers?symbol=ETHUSDT');
+  return parseFloat(res.data.result[0].last_price);
+}
+
+module.exports = { fetchBybitPrice };
 
