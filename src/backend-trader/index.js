55a93d8696eac49dddede6f03c62cdc41c3192db
CroakBot1 <apploverss1@gmail.com>
2025-07-21 09:17:35 +0800

    Update index.js

--- src/backend-trader/index.js
+++ src/backend-trader/index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const { checkPriceAndTrade } = require('./trader');
+const logger = require('./logger');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🐸 Croak Bot Live');
+});
+
+setInterval(async () => {
+  await checkPriceAndTrade();
+}, 10 * 1000); // every 10s
+
+app.listen(PORT, () => {
+  logger.info(`🚀 CROAK BOT listening on port ${PORT}`);
+});
 
