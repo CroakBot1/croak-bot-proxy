950b531add639543ceb4aa76c6f0df84ebdec137
CroakBot1 <apploverss1@gmail.com>
2025-07-23 07:09:09 +0800

    Update uniswapHelpers.js

--- src/backend-trader/uniswapHelpers.js
+++ src/backend-trader/uniswapHelpers.js
@@ -1,4 +1,50 @@
-// uniswapHelpers.js
+// ... other exports above
+
+// Import swapRouterAbi inside the same file
+const swapRouterAbi = [
+  'function exactInputSingle((address,address,uint24,address,uint256,uint256,uint160)) external payable returns (uint256)'
+];
+
+async function getSwapTx({ wallet, amountIn, tokenIn, tokenOut, slippage = 0.01 }) {
+  const router = new ethers.Contract(SWAP_ROUTER_ADDRESS, swapRouterAbi, wallet);
+  const fee = 500;
+  const rawAmountIn = toRaw(amountIn, 18);
+  const amountOutMinimum = 0;
+  const deadline = getDeadline();
+
+  const params = {
+    tokenIn,
+    tokenOut,
+    fee,
+    recipient: await wallet.getAddress(),
+    deadline,
+    amountIn: rawAmountIn,
+    amountOutMinimum,
+    sqrtPriceLimitX96: 0,
+  };
+
+  const iface = new ethers.utils.Interface(swapRouterAbi);
+  const data = iface.encodeFunctionData('exactInputSingle', [params]);
+
+  return {
+    to: SWAP_ROUTER_ADDRESS,
+    data,
+    value: tokenIn === ethers.constants.AddressZero ? rawAmountIn : 0,
+    gasLimit: ethers.utils.hexlify(700000),
+  };
+}
+
+module.exports = {
+  USDC_ADDRESS,
+  WETH_ADDRESS,
+  SWAP_ROUTER_ADDRESS,
+  SLIPPAGE_TOLERANCE,
+  getDeadline,
+  toRaw,
+  fromRaw,
+  getSwapTx, // 👈 Add this to exports
+};
+/ uniswapHelpers.js
 
 const { ethers } = require('ethers');
 
